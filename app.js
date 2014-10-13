3f08d3d2c0b35699f99850eb4bd2fb3e6b84aaf8
Chris Callahan <callahan.c@gmail.com>
2014-10-12 20:00:03 -0400

    basic roman-to-int conversion

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 var app = angular.module('romanNumerals', []);
 
-app.controller('romanController', function($scope) {
-    $scope.text = "hello world!";
+app.controller('RomanController', function($scope) {
+    $scope.roman = "MMXIV",
+    $scope.mappings = {
+        "M": 1000,
+        "D": 500,
+        "C": 100,
+        "L": 50,
+        "X": 10,
+        "V": 5,
+        "I": 1
+    },
+
+    $scope.deromanize = function() {
+        var arr = $scope.roman.split("");
+        var number = 0;
+
+        for ( var i = 0; i < arr.length; i++ ) {
+            if ( $scope.mappings[arr[i]] < $scope.mappings[arr[i + 1]] ) {
+                number -= $scope.mappings[arr[i]];
+            } else {
+                number += $scope.mappings[arr[i]];
+            }
+        }
+
+        return number;
+    }
 });
